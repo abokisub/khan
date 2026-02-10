a2c8b1fa268191e21e348852298aa7f868224738
abokisub <abokisub@gmail.com>
2026-02-10 16:44:15 +0100

    Fix: Resolve CreateAirtime_selTable duplicate class error

--- database/migrations/2025_06_24_223413_create_airtime_sel_table.php
+++ database/migrations/2025_06_24_223413_create_airtime_sel_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateAirtime_selTable extends Migration
+class CreateAirtimeSelTableFixed extends Migration
 {
     /**
      * Run the migrations.
